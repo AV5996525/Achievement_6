503f04f75661410bdc81613b93be43d79a5b7636
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-23 13:42:43 -0500

    comments

--- achieve_6.py
+++ achieve_6.py
@@ -1,5 +1,10 @@
+#Name:          achieve_6.py
+#Author:        AJ Varatharajan
+#Date Created:  February 22, 2023
+#Date Last Modified: February 23, 2023
+#Purpose: User is able to input a list of numbers and will have inputed numbers sorted as prime or non prime numbers.
 #
-#
+#This program will output the user generated number list as either prime or non prime numbers seperately. 
 numbers = (input("Enter your numbers")).split(",")
 for x in (numbers):
     x = int(x)
