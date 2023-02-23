bca732504115ddcef287dcbf57a1bbef46665ae9
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-22 20:42:49 -0500

    almost

--- achieve_6.py
+++ achieve_6.py
@@ -1,1 +1,15 @@
-
+numbers = (input("Enter your numbers")).split(",")
+for x in (numbers):
+    x = int(x)
+    flag = False
+    if x == 1:
+        print("The number {} is not a prime number".format(numbers))
+    elif x > 1 :
+        for i in range(2, x) :
+            if (x % i) == 0:
+                flag = True
+                break
+        if flag:
+            print("The number {} is not a prime number".format(numbers))
+        else:
+            print("The number {} is a prime number".format(numbers))
