e17cf24191a392f8bae2ed503c08e472b544eca4
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-22 21:16:18 -0500

    comments

--- achieve_6.py
+++ achieve_6.py
@@ -1,15 +1,20 @@
+#
+#
 numbers = (input("Enter your numbers")).split(",")
 for x in (numbers):
     x = int(x)
     flag = False
     if x == 1:
-        print("The number {} is not a prime number".format(numbers))
+        print("The number {} is not a prime number".format(x))
+        
     elif x > 1 :
         for i in range(2, x) :
             if (x % i) == 0:
                 flag = True
                 break
         if flag:
-            print("The number {} is not a prime number".format(numbers))
+            print("The number {} is not a prime number".format(x))
+            
         else:
-            print("The number {} is a prime number".format(numbers))
+            print("The number {} is a prime number".format(x))
+            
